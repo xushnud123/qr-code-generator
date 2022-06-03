a1a07c3e15af5cfc81b8a3399a46d79781c85317
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-06-03 13:48:51 +0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+              hello
+            </header>
+        </div>
+    );
+}
+
+export default App;
