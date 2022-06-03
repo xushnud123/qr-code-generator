43bf4727e021999ffd1375a6364c36c30f7db6bb
ziyoweb <ziyodulloyevm1@gmail.com>
2022-06-04 01:13:29 +0500

    group first

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 import Group from "./components/Group/group";
 import Toolbar from "./components/Toolbar/toolbar";
 
+const App = () => {
+  const [open, setOpen] = useState(false);
 
-const App = () =>{
-  const [open, setOpen] = useState(false)
-
-    return (
-      <div style={{ height: "100%" }}>
-        <Toolbar open={open} setOpen={setOpen}/>
-        <Group open={open}/>
-      </div>
-    );
-}
+  return (
+    <div style={{ height: "100%" }}>
+      <Toolbar open={open} setOpen={setOpen} />
+      <Group open={open} />
+    </div>
+  );
+};
 
 export default App;
