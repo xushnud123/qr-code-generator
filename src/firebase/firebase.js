b0859673a75f6c159d68362811e2ba0e6bebca57
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-07-27 00:15:20 +0500

    firebase key update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-})
+  apiKey: "AIzaSyCEY-XjfGbl37q9l6Xngsz0ECT3AD5hhVQ",
+  authDomain: "auth-development-b301b.firebaseapp.com",
+  projectId: "auth-development-b301b",
+  storageBucket: "auth-development-b301b.appspot.com",
+  messagingSenderId: "1089040293922",
+  appId: "1:1089040293922:web:15bc056d078cd0c388ea7b",
+  measurementId: "G-4VNMR40N2V",
+});
 
 export const auth = getAuth(app);
 export const createUser =  createUserWithEmailAndPassword()
